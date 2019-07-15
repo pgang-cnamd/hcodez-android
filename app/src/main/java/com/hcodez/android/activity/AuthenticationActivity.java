eee017e058d6fa6606bad8788873ad2d4b6c18a1
cezarmathe <cezarmathe@gmail.com>
2019-07-15 14:48:57 +0300

    Add splash screen

---
+++ app/src/main/java/com/hcodez/android/activity/AuthenticationActivity.java
@@ -0,0 +1,15 @@
+package com.hcodez.android.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hcodez.android.R;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
