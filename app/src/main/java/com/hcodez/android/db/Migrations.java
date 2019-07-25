481ca1c966aa7f7c2eb94fc811f6ccf0e4921f48
cezarmathe <cezarmathe@gmail.com>
2019-07-25 13:46:02 +0300

    Make Migrations final

--- app/src/main/java/com/hcodez/android/db/Migrations.java
+++ app/src/main/java/com/hcodez/android/db/Migrations.java
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
-public class Migrations {
+public final class Migrations {
 
     public static final Migration MIGRATION_1_2 = new Migration(1, 2) {
         @Override
