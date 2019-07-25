8c2b4db8c1c87edd746ea0adf66eef72033f3447
cezarmathe <cezarmathe@gmail.com>
2019-07-25 13:42:12 +0300

    Add search features for Code

--- app/src/main/java/com/hcodez/android/db/dao/CodeDao.java
+++ app/src/main/java/com/hcodez/android/db/dao/CodeDao.java
@@ -23,10 +23,10 @@ public interface CodeDao {
     void insert(CodeEntity code);
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insertAll(List<CodeEntity> codes);
+    void insertAll(List<CodeEntity> code);
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insertAll(CodeEntity... codes);
+    void insertAll(CodeEntity... code);
 
     @Query("SELECT * FROM code WHERE id = :codeId")
     LiveData<CodeEntity> loadCode(int codeId);
@@ -36,6 +36,14 @@ public interface CodeDao {
 
     @Query("SELECT * FROM code WHERE identifier IN (:identifiers)")
     LiveData<List<CodeEntity>> loadCodesWithIdentifier(List<String> identifiers);
+
+    @Query("SELECT code.* FROM code JOIN codeFts ON (code.id = codeFts.rowid) "
+            + "WHERE codeFts MATCH :query")
+    LiveData<List<CodeEntity>> searchAllProducts(String query);
+
+    @Query("SELECT code.* FROM code JOIN codeFts ON (code.id = codeFts.rowid) "
+            + "WHERE codeFts MATCH :query")
+    List<CodeEntity> searchAllProductsSync(String query);
 
     @Delete
     void delete(CodeEntity code);
