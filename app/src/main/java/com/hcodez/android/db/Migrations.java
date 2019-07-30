b843f034d8d9d43c844007756d42c672fcf4134f
cezarmathe <cezarmathe@gmail.com>
2019-07-30 16:31:49 +0300

    Add new migration

--- app/src/main/java/com/hcodez/android/db/Migrations.java
+++ app/src/main/java/com/hcodez/android/db/Migrations.java
@@ -10,7 +10,7 @@ public final class Migrations {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase database) {
             database.execSQL("CREATE VIRTUAL TABLE IF NOT EXISTS `codeFts` USING FTS4(" +
-                    "`rowid` INTEGER, `identifier` TEXT, `owner` TEXT, `name` TEXT");
+                    "`rowid` INTEGER, `identifier` TEXT, `owner` TEXT, `name` TEXT)");
             database.execSQL("INSERT INTO codeFts (`rowid`, `identifier`, `owner`, `passcode`, `name`)" +
                     "SELECT `id`, `identifier`, `owner`, `name` FROM code");
         }
@@ -19,7 +19,13 @@ public final class Migrations {
     public static final Migration MIGRATION_2_3 = new Migration(2,3) {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase database) {
-            // TODO: 2019-07-30 implement
+            database.execSQL("CREATE TABLE IF NOT EXISTS `content`(" +
+                    "`id` INTEGER, `identifier` TEXT, `owner` TEXT, `name` TEXT)");
+
+            database.execSQL("CREATE VIRTUAL TABLE IF NOT EXISTS `contentFts` USING FTS4(" +
+                    "`rowid` INTEGER, `description` TEXT, `resource_uri` TEXT)");
+            database.execSQL("INSERT INTO contentFts (`rowid` INTEGER, `description` TEXT, `resource_uri` TEXT)" +
+                    "SELECT `id`, `description`, `resource_uri` FROM content");
         }
     };
 }
