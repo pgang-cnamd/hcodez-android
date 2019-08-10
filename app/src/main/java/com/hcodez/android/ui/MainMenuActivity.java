0fd02c5cb7cfe95e69323bc33c294e524e8375e1
burnscopesali <crutoi2@yahoo.com>
2019-08-10 23:06:44 +0300

    Add code for RecyclerView

--- app/src/main/java/com/hcodez/android/ui/MainMenuActivity.java
+++ app/src/main/java/com/hcodez/android/ui/MainMenuActivity.java
@@ -7,14 +7,23 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.SearchView;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.hcodez.android.R;
+import com.hcodez.android.ui.adapter.CodeAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainMenuActivity extends Activity {
 
     private FloatingActionButton mAddCodeFloatingActionButton;
     private FloatingActionButton mFindCodeFloatingActionButton;
     private SearchView           mCodeSearchView;
+    private RecyclerView         mCodeListRecyclerView;
+    private CodeAdapter          mCodeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,32 @@ public class MainMenuActivity extends Activity {
         mAddCodeFloatingActionButton  = findViewById(R.id.buttonAdd);
         mFindCodeFloatingActionButton = findViewById(R.id.buttonFind);
         mCodeSearchView               = findViewById(R.id.codeSearch);
+        mCodeListRecyclerView         = findViewById(R.id.codeList);
+        mCodeAdapter                  = new CodeAdapter();
+
+        mCodeListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mCodeListRecyclerView.setAdapter(mCodeAdapter);
+
+        /**
+         * List made as an simple demonstration
+         */
+        List<String> listExample = new ArrayList<>();
+        listExample.add("code1");
+        listExample.add("code2");
+        listExample.add("code3");
+        listExample.add("code4");
+
+        List<String> list = new ArrayList<>();
+        list.addAll(listExample);
+        list.addAll(listExample);
+        list.addAll(listExample);
+        list.addAll(listExample);
+        list.addAll(listExample);
+        list.addAll(listExample);
+        list.addAll(listExample);
+        list.addAll(listExample);
+
+        mCodeAdapter.setItems(list);
 
         mAddCodeFloatingActionButton.setOnClickListener(view -> startActivity(new Intent(MainMenuActivity.this, CodeAddActivity.class)));
 
@@ -32,8 +67,11 @@ public class MainMenuActivity extends Activity {
         mCodeSearchView.setOnClickListener(view -> mCodeSearchView.setIconified(false));
     }
 
+    /**
+     * Method used for hiding the keyboard when touching outside the text
+     */
     public void hideKeyboard(View view) {
-        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
--- app/src/main/java/com/hcodez/android/ui/adapter/CodeAdapter.java
+++ app/src/main/java/com/hcodez/android/ui/adapter/CodeAdapter.java
@@ -1,34 +1,70 @@
 package com.hcodez.android.ui.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.hcodez.android.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CodeAdapter extends RecyclerView.Adapter<CodeAdapter.CodeViewHolder> {
 
+    private List<String> mCodeItems = new ArrayList<>();
+
+    public void setItems(List<String> items) {
+        mCodeItems = items;
+        notifyDataSetChanged();
+    }
 
     @NonNull
     @Override
     public CodeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        if (viewType == 0)
+            return CodeViewHolder.inflate(parent);
+        else
+            return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull CodeViewHolder holder, int position) {
-
+        if (holder instanceof CodeViewHolder) {
+            holder.bind(mCodeItems.get(position));
+        }
     }
 
     @Override
     public int getItemCount() {
+        return mCodeItems.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
         return 0;
     }
 
     static class CodeViewHolder extends RecyclerView.ViewHolder {
 
+        private TextView mTextView;
+
         public CodeViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            mTextView = itemView.findViewById(R.id.list_item);
+        }
+
+        public static CodeViewHolder inflate(ViewGroup parent) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_codes, parent, false);
+            return new CodeViewHolder(view);
+        }
+
+        public void bind(String text) {
+            mTextView.setText(text);
         }
     }
 }
