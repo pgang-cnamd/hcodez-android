ea44547af2eee7582bdf0f0ed2c6a32cb4b750d1
cezarmathe <cezarmathe@gmail.com>
2019-09-01 18:25:10 +0300

    Add new activities

---
+++ app/src/main/java/com/hcodez/android/ui/EnterTextContentActivity.java
@@ -0,0 +1,16 @@
+package com.hcodez.android.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hcodez.android.R;
+
+public class EnterTextContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_text_content);
+    }
+}
